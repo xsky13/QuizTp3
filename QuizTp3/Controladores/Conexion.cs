09b567b9f16347e34d74ecc561159409e8cf6408
Jared Peter <aljapeter05@gmail.com>
2025-06-02 13:33:19 -0300

    Setup db

---
+++ QuizTp3/Controladores/Conexion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace QuizTp3.Controladores
+{
+    internal class Conexion
+    {
+        static string connectionString = $"Data Source=../../../database.db";
+        static SQLiteConnection conexion = new SQLiteConnection(connectionString);
+        public static void OpenConnection()
+        {
+            if (conexion.State == System.Data.ConnectionState.Closed)
+                conexion.Open();
+            //Activamos la gestión de llaves foráneas
+            using (SQLiteCommand command = new SQLiteCommand("PRAGMA foreign_keys = ON;", conexion))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+        public static void CloseConnection()
+        {
+            conexion.Close();
+        }
+        public static SQLiteConnection Connection
+        {
+            set { conexion = value; }
+            get { return conexion; }
+        }
+    }
+}
--- QuizTp3/Program.cs
+++ QuizTp3/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace QuizTp3
+﻿using System.Data.SQLite;
+using QuizTp3.Controladores;
+
+namespace QuizTp3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            Conexion.OpenConnection();
+
             Console.WriteLine("Hello, World!");
+
+            Conexion.CloseConnection();
         }
     }
 }
