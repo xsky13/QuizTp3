b4106090ef6b4113f43cf02dff6f0d878fa1d369
Jared Peter <aljapeter05@gmail.com>
2025-06-02 14:26:10 -0300

    empeze autenticacion

---
+++ QuizTp3/Controladores/Autenticacion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Libreria2025;
+
+namespace QuizTp3.Controladores
+{
+    internal class Autenticacion
+    {
+
+        public static void Registrarse()
+        {
+            Console.Write("Ingrese su usuario");
+            string usuario = Console.ReadLine();
+
+
+            Console.Write("Ingrese su contrasena: ");
+            string pwd = Console.ReadLine();
+
+
+        }
+    }
+}
